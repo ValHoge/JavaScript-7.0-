c519ee16095744c71e91d0bec44c34a52a99b297
mikha1lova <47481575+mikha1lova@users.noreply.github.com>
2019-04-24 01:39:54 +1000

    Add files via upload

---
+++ lesson-1/easy/js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let money = prompt('Ваш бюджет?','');
+let time = prompt('Введите дату в формате YYYY-MM-DD','');
+let с1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let h1 = +prompt('Во сколько обойдется?', '');
+let с2 = prompt('Введите обязательную статью расходов в этом месяцу', '');
+let h2 = +prompt('Во сколько обойдется?', '');
+
+let appData = {
+  budget : money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses : '',
+  income : '',
+  savings : false
+ };
+
+appData.money = money;
+appData.time = time;
+appData.expenses[c1] = h1;
+appData.expenses[c2] = h2;
+
+let budgetOfOneDay = alert(money/30);
+console.log(money);
+console.log(time);
+console.log(budgetOfOneDay);
---
+++ lesson-1/easy/js/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let money = prompt('Ваш бюджет?');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+
+
+
+ let appData = {
+   
+ };
+ money.appData = '';
+ time.appData = '';
+ 
+console.log(money);
+console.log(time);
