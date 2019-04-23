c8cb02828b594a23e0042ecf7f2d34ca23fad926
mikha1lova <47481575+mikha1lova@users.noreply.github.com>
2019-04-24 02:37:02 +1000

    Обязательное задание 1-го урока. Все сделано.

--- lesson-1/easy/js/script.js
+++ lesson-1/easy/js/script.js
@@ -2,7 +2,7 @@
 
 let money = prompt('Ваш бюджет?','');
 let time = prompt('Введите дату в формате YYYY-MM-DD','');
-let с1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let с1 = prompt('Введите обязательную статью расходов в этом месяцу', '');
 let h1 = +prompt('Во сколько обойдется?', '');
 let с2 = prompt('Введите обязательную статью расходов в этом месяцу', '');
 let h2 = +prompt('Во сколько обойдется?', '');
@@ -18,10 +18,12 @@ let appData = {
 
 appData.money = money;
 appData.time = time;
-appData.expenses[c1] = h1;
-appData.expenses[c2] = h2;
+appData.expenses.c1 = h1;
+appData.expenses.c2 = h2;
 
-let budgetOfOneDay = alert(money/30);
+let budgetOfOneDay = money / 30;
+alert(budgetOfOneDay)
+
 console.log(money);
 console.log(time);
 console.log(budgetOfOneDay);
